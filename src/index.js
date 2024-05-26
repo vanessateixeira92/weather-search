4f75ea2e85d5d68e98ad3972dcfc65a52880c5c4
vanessateixeira92 <vanessa.r.m.teixeira@gmail.com>
2024-05-26 16:13:26 +0100

    Injected a image-icon with API

--- src/index.js
+++ src/index.js
@@ -7,13 +7,16 @@ function displayTemperature(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
-  console.log(response.data);
+  let iconElement = document.querySelector("#temperature-icon");
+
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
+
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="current-temperature-icon"/>`;
 }
 
 function formatDate(date) {
